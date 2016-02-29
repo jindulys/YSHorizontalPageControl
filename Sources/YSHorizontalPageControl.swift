9228274473321ea930106c3518b6ed22c18de413
yansong li <857367901@qq.com>
2016-02-29 18:13:56 -0500

    First commit

---
+++ Sources/YSHorizontalPageControl.swift
@@ -0,0 +1,9 @@
+//
+//  YSHorizontalPageControl.swift
+//  YSHorizontalPageControl
+//
+//  Created by yansong li on 2016-02-29.
+//  Copyright © 2016 yansong li. All rights reserved.
+//
+
+import Foundation
---
+++ YSHorizontalPageControl/YSHorizontalPageControl.h
@@ -0,0 +1,19 @@
+//
+//  YSHorizontalPageControl.h
+//  YSHorizontalPageControl
+//
+//  Created by yansong li on 2016-02-29.
+//  Copyright © 2016 yansong li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YSHorizontalPageControl.
+FOUNDATION_EXPORT double YSHorizontalPageControlVersionNumber;
+
+//! Project version string for YSHorizontalPageControl.
+FOUNDATION_EXPORT const unsigned char YSHorizontalPageControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YSHorizontalPageControl/PublicHeader.h>
+
+
